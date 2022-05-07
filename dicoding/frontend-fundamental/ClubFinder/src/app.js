9ca87dd239aed3a2880b365a53751e18213dfdef
Audie Milson <104993633+audie38@users.noreply.github.com>
2022-05-07 13:46:51 +0700

    WebPack

---
+++ dicoding/frontend-fundamental/ClubFinder/src/app.js
@@ -0,0 +1,6 @@
+import "regenerator-runtime";
+import main from "./script/view/main.js";
+import "./script/component/app-bar.js";
+import "./styles/style.css";
+
+document.addEventListener("DOMContentLoaded", main);
