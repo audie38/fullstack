cc9ba793579699cc3f3af2897045aa1ca048397e
Audie Milson <104993633+audie38@users.noreply.github.com>
2022-05-08 21:00:38 +0700

    Vue2 Create Component

--- codepolitan/vue2-basic/app.js
+++ codepolitan/vue2-basic/app.js
@@ -1,3 +1,60 @@
+Vue.component("price", {
+  data: function () {
+    return {};
+  },
+  props: {
+    value: Number,
+    prefix: {
+      type: String,
+      default: "$",
+    },
+    precision: {
+      type: Number,
+      default: 2,
+    },
+  },
+  template: `
+  <span>{{this.prefix + Number.parseFloat(this.value).toFixed(this.precision) }}</span>
+  `,
+});
+
+Vue.component("product-list", {
+  props: ["products", "maximum"],
+  methods: {},
+  template: `  
+  <div class="row">
+    <div
+      v-if="item.price <= Number(maximum)"
+      class="col-md-3 col-sm-6 my-2"
+      v-for="(item, index) in products"
+      :key="index"
+    >
+      <div class="card mx-2">
+        <img
+          :src="item.image"
+          class="card-img-top img-fluid"
+          :alt="item.name"
+          loading="lazy"
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{item.name}}</h5>
+          <p class="card-text">{{item.description}}</p>
+          <p class="card-text">
+            <price :value="Number(item.price)" />
+          </p>
+          <button
+            @click="$emit('add', item)"
+            class="btn btn-primary w-100"
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  `,
+});
+
 const app = new Vue({
   el: "#app",
   data: {
