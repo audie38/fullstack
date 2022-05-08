d166a4e9cd8a36f3c4c01f0a5417469d9bc59796
Audie Milson <104993633+audie38@users.noreply.github.com>
2022-05-08 19:40:50 +0700

    Extend and Override

--- codepolitan/php-oop-basic/chapter2.php
+++ codepolitan/php-oop-basic/chapter2.php
@@ -7,5 +7,9 @@ $cust = new Customer;
 $cust->firstName = "Audie";
 $cust->lastName = "Milson";
 $cust->userName = "audie.milson";
+$cust->city = "Jakarta";
+$cust->country = "Indonesia";
 
 echo $cust->getFullName();
+echo "<br/>";
+echo $cust->getLocation();
--- codepolitan/php-oop-basic/model/Customer.php
+++ codepolitan/php-oop-basic/model/Customer.php
@@ -5,4 +5,18 @@ include("User.php");
 // Inheritance
 class Customer extends User
 {
+    // Extend
+    var $city;
+    var $country;
+
+    function getLocation()
+    {
+        return $this->city . ", " . $this->country;
+    }
+
+    // Override
+    function getFullName()
+    {
+        return $this->firstName . " " . $this->lastName . " (customer)";
+    }
 }
