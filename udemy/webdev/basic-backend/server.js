3b45f6273cf2e274ebb1fa790b0f05546ca23fc2
Audie Milson <104993633+audie38@users.noreply.github.com>
2022-05-28 13:18:00 +0700

    EJS TodoList

--- udemy/webdev/basic-backend/server.js
+++ udemy/webdev/basic-backend/server.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const app = express();
-
-dotenv.config();
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
+const app = express();
+const todoItems = [];
+
+app.set("view engine", "ejs");
+app.set("view cache", false);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("OK!");
+  let title = "EJS TodoList";
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = new Date().toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    webTitle: title,
+    kindOfDay: day,
+    listOfTodos: todoItems,
+  });
+});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  todoItems.push(item);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
