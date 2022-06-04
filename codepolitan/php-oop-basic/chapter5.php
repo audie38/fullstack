1c57703fb11e55b05368f0980244bdc966f3e991
Audie Milson <104993633+audie38@users.noreply.github.com>
2022-06-04 20:11:37 +0700

    Construct Argument v1

--- codepolitan/php-oop-basic/chapter5.php
+++ codepolitan/php-oop-basic/chapter5.php
@@ -4,6 +4,7 @@ include("./model/part2/Student.php");
 include("./model/part2/Elementary.php");
 include("./model/part2/Junior.php");
 include("./model/part2/Senior.php");
+include("./model/part2/Product.php");
 
 $elementary = new Elementary;
 echo "Elementary: {$elementary->total} <br/>";
@@ -12,4 +13,7 @@ echo "Junior: {$junior->total} <br/>";
 $senior = new Senior;
 echo "Senior: {$senior->total} <br/>";
 
-echo "Instance Count: " . Student::$instanceCount;
+echo "Instance Count: " . Student::$instanceCount . "<br/>";
+
+$shirt = new Product("T-Shirt", "Blue");
+echo json_encode($shirt);
---
+++ codepolitan/php-oop-basic/model/part2/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+class Product
+{
+    public $name;
+    public $color;
+
+    public function __construct($name, $color)
+    {
+        $this->name = $name;
+        $this->color = $color;
+    }
+}
